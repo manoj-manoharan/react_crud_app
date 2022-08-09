33e2cc5c037c18bc18a5d7ea53e095721fc69058
Manoj <yogeshwaranmanoharan@gmail.com>
2022-08-09 09:44:22 +0530

    - Content overflow issue fixed

--- client/src/components/UX/Post/ViewPost.tsx
+++ client/src/components/UX/Post/ViewPost.tsx
@@ -30,7 +30,7 @@ export const ViewPost: FC<{ postId: PostType['id'] }> = ({postId}) => {
                 <div className="m-4">
                     <h1 className="text-3xl">{post.title}</h1>
                     <small className="font-extralight">By User #{post.id}</small>
-                    <p>{post.body}</p>
+                    <p className="max-h-96 overflow-y-scroll">{post.body}</p>
 
                 </div>
             </Modal>
