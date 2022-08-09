f13b93c5c70a42b02bc885c0f373bebd6bc90f72
Manoj <yogeshwaranmanoharan@gmail.com>
2022-08-09 09:27:36 +0530

    - Changed varchar to text for body

--- server/database/migrations/2022_08_05_165744_create_posts_table.php
+++ server/database/migrations/2022_08_05_165744_create_posts_table.php
@@ -16,7 +16,7 @@ return new class extends Migration {
             $table->id();
             $table->integer('userId');
             $table->string('title');
-            $table->string('body');
+            $table->text('body');
         });
     }
 
